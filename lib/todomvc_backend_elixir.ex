0380bde74002801537273f8bbf4c387e351d6d84
Dan Stutzman <dan@danstutzman.com>
2016-08-30 11:21:04 -0600

    Now using defstruct so can access port with options.port.

--- lib/todomvc_backend_elixir.ex
+++ lib/todomvc_backend_elixir.ex
@@ -1,29 +1,35 @@
 defmodule TodomvcBackendElixir do
   use Application
 
+  defmodule Options do
+    defstruct []
+
+    def parse_argv do
+      {parsed, remaining, invalid} = OptionParser.parse(System.argv, switches: [
+        port: :integer,
+      ])
+      if remaining != [] or invalid != [] do
+        raise "Couldn't understand command-line args: #{inspect(System.argv)}"
+      end
+
+      {:done, options} = Enumerable.reduce parsed, {:cont, %Options{}},
+        fn({field, value}, accum) -> {:cont, Map.put(accum, field, value)} end
+
+      if options.port == nil do raise "Missing --port option" end
+
+      options
+    end
+  end
+
   def start(_type, _args) do
-    import Supervisor.Spec, warn: false
+    options = Options.parse_argv
 
-    IO.puts :stderr, "args is #{inspect(System.argv)}"
-    {options, remaining_args, invalid_options} = OptionParser.parse(System.argv,
-      switches: [port: :integer])
-    if remaining_args != [] or invalid_options != [] do
-      raise "Couldn't understand command-line args: #{inspect(System.argv)}"
-    end
-    port = options[:port]
-    if port == nil do
-      raise "Missing --port option"
-    end
-    IO.puts :stderr, "Running web server on port #{port}..."
-
+    IO.puts :stderr, "Running web server on port #{options.port}..."
     Supervisor.start_link([
-      worker(TodomvcBackendElixir.Repo, []),
+      Supervisor.Spec.worker(TodomvcBackendElixir.Repo, []),
       Plug.Adapters.Cowboy.child_spec(
-        :http, TodomvcBackendElixir.Router, [], [port: port])
-    ], [
-      strategy: :one_for_one,
-      name: TodomvcBackendElixir.Supervisor
-    ])
+        :http, TodomvcBackendElixir.Router, [], [port: options.port])
+    ], [strategy: :one_for_one, name: TodomvcBackendElixir.Supervisor])
   end
 
 end
